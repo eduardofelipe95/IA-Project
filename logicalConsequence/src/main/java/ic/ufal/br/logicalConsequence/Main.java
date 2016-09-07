57cef492f8f6ab2ae37963256b45697fd41c862e
Eduardo Felipe de Souza <eduardofelipe@ic.ufal.br>
2016-09-07 17:41:11 -0300

    initial commit

---
+++ logicalConsequence/src/main/java/ic/ufal/br/logicalConsequence/Main.java
@@ -0,0 +1,13 @@
+package ic.ufal.br.logicalConsequence;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
