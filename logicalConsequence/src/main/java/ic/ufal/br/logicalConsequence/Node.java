d507b4711cb1a78168a6a4b3cd924995a57ef29d
Eduardo Felipe <eduardofelipe@ic.ufal.br>
2016-09-13 22:37:18 -0300

    create class node

---
+++ logicalConsequence/src/main/java/ic/ufal/br/logicalConsequence/Node.java
@@ -0,0 +1,29 @@
+package ic.ufal.br.logicalConsequence;
+
+public class Node {
+	protected String token;
+	protected Node left;
+	protected Node right;
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public Node getLeft() {
+		return left;
+	}
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public void setRight(Node right) {
+		this.right = right;
+	}
+	
+	
+	
+
+}
